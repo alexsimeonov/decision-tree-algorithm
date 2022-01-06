7eff6139595c1b30c9dcfe7e7fd3c36e076d05db
Alexander Simeonov <alex.simeonov@hotmail.com>
2022-01-06 22:02:35 +0200

    Fix criterion user input validation

--- node.py
+++ node.py
@@ -7,10 +7,14 @@ Created on Tue Dec  7 23:38:36 2021
 from enum import Enum
 import uuid
 
+# Currently having problems with following lines - ask for some help -> why it says that gnrl is missing
+#from binning.aislab.dp_feng.binenc import *
+#from binning.aislab.gnrl import *
+
 # The node status -> could be improved and make sure it works
 class Status(Enum):
-    TERMINAL = 1
-    LEAF = 2
+    TERMINAL = 0
+    LEAF = 1
 
 class Node:
     def __init__(self, data_set: list, params: list, parent=None):
@@ -20,12 +24,16 @@ class Node:
         self.params = params
         self.parent = parent # If present
         # Ask if the child nodes should be limited to 2 and if so -> is the following design appropriate
+        # Else use a list 'children' maybe
         self.left = None
         self.right = None
         self.status = Status.TERMINAL
     
     def grow(self, growing_method):
         print("growing")
+        
+            # When fix the import use the ubng method in here
+        
             # Recursively executed the actions function described below
             # Call the external function (split_method) from main.py 
             # with self.data_set use the result in order to create a new Node instances
