1e3091b4932cc1d964077ed31bf45cd016ca1139
Alexander Simeonov <alex.simeonov@hotmail.com>
2022-01-11 22:34:15 +0200

    Import real dataset and add few comments

--- dt_algorithm.py
+++ dt_algorithm.py
@@ -7,6 +7,9 @@ Created on Fri Dec 17 00:23:35 2021
 
 from node import Node
 from enum import Enum
+import pandas as pd
+
+csv_file = 'C:/University/Дипломна работа/CODE/decision-tree-algorithm/binning/aislab/data/cnf_LC.csv'
 
 class Criterion(Enum):
     GINI = "gini"
@@ -18,6 +21,8 @@ criterion = input("Enter the name of function to measure split quality(Could be 
 max_depth = input("Enter desired max depth of decision tree: ")
 min_samples_split = input("Enter minimum samples in a node after split: ")
 min_samples_leaf = input("Enter minimum samples in a leaf node: ")
+max_children_count = input("Enter maximum number of children per node: ")
+# add max number of child nodes
 
 # Here the imported dataset should be passed to the root node, along with the rest of the desired
 # hyperparameters. The result should be stored in a variable in order to be used/visualised at the end.
@@ -29,6 +34,8 @@ hyperparams = {}
 values = set(item.value for item in Criterion)
 
 if criterion:
+    # instead of default value use error handling
+    # Please send me a list of the criterions supported by the algorithm in order to add them and error handling
     hyperparams["criterion"] = Criterion(criterion) if (criterion in values) else Criterion.GINI
 if max_depth:
     hyperparams["max_depth"] = max_depth
@@ -36,8 +43,10 @@ if min_samples_split:
     hyperparams["min_samples_split"] = min_samples_split
 if min_samples_leaf:
     hyperparams["min_samples_leaf"] = min_samples_leaf
+if max_children_count:
+    hyperparams["max_children_count"] = max_children_count
     
-dataset = ['one', 'two', 'three'] # Will contain the initial dataset
+dataset = pd.read_csv(csv_file) # Will contain the initial dataset
     
 # Creating the root node from data provided along with user input.
-root_node = Node(dataset, hyperparams)
+root_node = Node(hyperparams, dataset)
--- node.py
+++ node.py
@@ -7,31 +7,26 @@ Created on Tue Dec  7 23:38:36 2021
 from enum import Enum
 import uuid
 
-# Currently having problems with following lines - ask for some help -> why it says that gnrl is missing
-#from binning.aislab.dp_feng.binenc import *
-#from binning.aislab.gnrl import *
+#from aislab.dp_feng.binenc import *
+#from aislab.gnrl import *
+from binning.aislab import *
 
-# The node status -> could be improved and make sure it works
 class Status(Enum):
-    TERMINAL = 0
-    LEAF = 1
+    DECISION = 0
+    TERMINAL = 1
+    LEAF = 2
 
 class Node:
-    def __init__(self, data_set: list, params: list, parent=None):
-        # The results from ub
+    def __init__(self, params: list, data_set=None, parent=None):
         self.id = uuid.uuid1() # Generates a UUID based on the host ID and current time
         self.data_set = data_set
         self.params = params
         self.parent = parent # If present
-        # Ask if the child nodes should be limited to 2 and if so -> is the following design appropriate
-        # Else use a list 'children' maybe
-        self.left = None
-        self.right = None
+        self.children = None # Add children after split
         self.status = Status.TERMINAL
     
     def grow(self, growing_method):
         print("growing")
-        
             # When fix the import use the ubng method in here
         
             # Recursively executed the actions function described below
