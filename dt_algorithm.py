ec1a495bfcf2bb95d50c981f43c1b2b070a1a112
Alexander Simeonov <alex.simeonov@hotmail.com>
2022-05-06 15:50:42 +0300

    Add DecisionTree class, refactor imports and library usage

--- dt_algorithm.py
+++ dt_algorithm.py
@@ -4,17 +4,29 @@ Created on Fri Dec 17 00:23:35 2021
 
 @author: alexs
 """
+import os
+import sys
 
 from node import Node
+from decision_tree import DecisionTree
 from enum import Enum
-import pandas as pd
 
-csv_file = 'C:/University/Дипломна работа/CODE/decision-tree-algorithm/binning/aislab/data/cnf_LC.csv'
+wpath = '/Users/alexandersimeonov/Library/Python/2.7/lib/python/site-packages'
+# path to dataset
+x_path = '/Users/alexandersimeonov/Documents/Development/University/decision-tree-algorithm/binning/aislab/data/x_samples_250000.csv'
+# path to output values
+y_path = '/Users/alexandersimeonov/Documents/Development/University/decision-tree-algorithm/binning/aislab/data/y_samples_250000.csv'
+c_path = '/Users/alexandersimeonov/Documents/Development/University/decision-tree-algorithm/binning/aislab/data/cnf_LC.csv'
+
+sys.path.append(wpath)
+os.chdir(wpath)
+
+# csv_file = '/Users/alexandersimeonov/Documents/Development/University/decision-tree-algorithm/binning/aislab/data/cnf_LC.csv'
 
 class Criterion(Enum):
     GINI = "gini"
     ENTROPY = "entropy"
-    
+
 print("The Criterion used by the binning algorithm is Chi2")
 max_depth = input("Enter desired max depth of decision tree: ")
 min_samples_split = input("Enter minimum samples in a node after split: ")
@@ -22,6 +34,8 @@ min_samples_leaf = input("Enter minimum samples in a leaf node: ")
 max_children_count = input("Enter maximum number of children per node: ")
 
 hyperparams = {}
+
+# DT class
 
 if max_depth:
     hyperparams["max_depth"] = max_depth
@@ -31,11 +45,13 @@ if min_samples_leaf:
     hyperparams["min_samples_leaf"] = min_samples_leaf
 if max_children_count:
     hyperparams["max_children_count"] = max_children_count
-    
-initial_dataset = pd.read_csv(csv_file)
-    
-# Creating the root node from data provided along with user input.
-root_node = Node(hyperparams, initial_dataset)
 
-print("Root node ready. Growing starts.")
-root_node.grow();
+# initial_dataset = pd.read_csv(csv_file)
+
+# Creating the root node from data provided along with user input.
+# Load config, x and y
+# x and y should be passed from the genarator if such i used, if not - use hardcoded from ready dataset
+# root_node = Node(hyperparams, initial_dataset)
+
+decision_tree = DecisionTree(x_path, y_path, c_path, hyperparams,)
+decision_tree.grow()
