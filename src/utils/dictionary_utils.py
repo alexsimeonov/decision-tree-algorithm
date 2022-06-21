ad52ae93e5a76b1e0e2cf307a4f785c7acca63de
Alexander Simeonov <alex.simeonov@hotmail.com>
2022-06-22 01:52:58 +0300

    Remove redundant comments from util function

--- src/utils/dictionary_utils.py
+++ src/utils/dictionary_utils.py
@@ -3,8 +3,6 @@ def filter_dictionary(dictionary, condition):
 
     for (key, value) in dictionary.items():
         if condition(value):
-            # currently the keys are not in order and taken from the initial dictionary
-            # do we have to keep the data in dictionary again or we can simply parse it to list instead?
             filtered_dictionary[key] = value
 
     return filtered_dictionary
